455ee432d12f859efdf0598e2a5666defb776c7f
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-21 00:28:59 +0530

    vercel analytics implemented

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import { Toaster } from 'react-hot-toast';
 import { AuthContextProvider } from './context/authContext';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <AuthContextProvider>
     <App/>
     <Toaster/>
+    <Analytics />
     </AuthContextProvider>
     
 );
