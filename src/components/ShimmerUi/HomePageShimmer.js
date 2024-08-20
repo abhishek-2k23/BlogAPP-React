13d152a050647018b32d52503e25410323026785
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-21 00:28:27 +0530

    media query fixed

--- src/components/ShimmerUi/HomePageShimmer.js
+++ src/components/ShimmerUi/HomePageShimmer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PostShimmer from './PostShimmer'
 function HomePageShimmer() {
   return (
-    <div >
+    <div className='md:w-10/12'>
           {/* {console.log(post)} */}
           {Array(2).fill("").map((index) => ( 
             <PostShimmer key={index}></PostShimmer>
