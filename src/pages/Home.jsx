4b9da5073d9f150da8145521069e208631cf2478
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-21 00:07:51 +0530

    media queries

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { toast } from "react-hot-toast";
-
+import HomePageShimer from '../components/ShimmerUi/HomePageShimmer';
 const Home = () => {
   const [post, setPost] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -30,16 +30,16 @@ const Home = () => {
   }, [cat]);
 
   return (
-    <div className=" flex justify-center md:px-2 lg:px-0">
+    <div className=" flex justify-center px-3 md:px-2 lg:px-0">
       {loading ? (
-        <Loading></Loading>
+        <HomePageShimer />
       ) : post.length === 0 ? (
         <div className="flex flex-col justify-center items-center min-h-[65vh] max-h-screen">
           <div className="font-bold text-lg">No Post Available</div>
           <div className="underline font-semibold text-lg text-green-800 cursor-pointer"><Link to="/write">Write</Link></div>{" "}
         </div>
       ) : (
-        <div className={`lg:w-10/12 px-2 md:px-0  space-y-12 flex flex-col h-auto min-h-[75vh] `}>
+        <div className={`lg:w-10/12 sm:px-0 px-3 md:px-0  space-y-12 flex flex-col h-auto min-h-[75vh] `}>
           {/* {console.log(post)} */}
           {post && post.map((post) => (
             <PostDesign post={post} key={post._id}></PostDesign>
