c8df7123961b6b35a0adc7832007d9140f997470
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-31 22:32:08 +0530

    selected category info

--- src/redux/slices/nav.slice.js
+++ src/redux/slices/nav.slice.js
@@ -4,14 +4,18 @@ const navSlice = createSlice({
     name: 'nav',
     initialState: {
         showSliderMenu: false,
+        selectedCategory: 'none'
     },
     reducers: {
         setShowSlider( state, action) {
             state.showSliderMenu = action.payload;
+        },
+        setSelectedCategory(state, action){
+            state.selectedCategory = action.payload;
         }
     }
 })
 
-export const  {setShowSlider} = navSlice.actions;
+export const  {setShowSlider, setSelectedCategory} = navSlice.actions;
 
 export default navSlice.reducer;
